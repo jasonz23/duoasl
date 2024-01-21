0b31c14daebf928282fe014b5bcf120a6089a618
Jason Zhao <jason1382311@gmail.com>
2024-01-20 18:44:07 -0800

    add logics

--- src/organisms/activityPage/ActivityPage.tsx
+++ src/organisms/activityPage/ActivityPage.tsx
@@ -28,7 +28,7 @@ const ACTIVITIES = [
 const SuccessModal = () => {
   const router = useRouter();
   return (
-    <div className="absolute left-1/2 top-1/2 flex h-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center gap-3 rounded-lg border-2 border-black bg-white sm:w-1/2">
+    <div className="absolute left-1/2 top-1/2 z-50 flex h-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center gap-3 rounded-lg border-2 border-black bg-white sm:w-1/2">
       <div className="">You Did It!</div>
       <Image
         src="/images/frog/main-frog.png"
@@ -55,7 +55,7 @@ const FailedModal = (props: FailedModalProps) => {
   const { closeModal, switchToLearn } = props;
 
   return (
-    <div className="absolute left-1/2 top-1/2 flex h-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center gap-3 rounded-lg border-2 border-black bg-white sm:w-1/2">
+    <div className="absolute left-1/2 top-1/2 z-50 flex h-1/2 w-full -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center gap-3 rounded-lg border-2 border-black bg-white sm:w-1/2">
       <div className="">Not Quite Right</div>
       <Image
         src="/images/frog/failed.png"
@@ -86,9 +86,10 @@ const ActivityPage = (props: ActivityPageProps) => {
   const [loadingCounter, setLoadingCounter] = React.useState(3);
   const [loadingCounterFlag, setLoadingCounterFlag] = React.useState(false);
   const [showSuccessModal, setShowSuccessModal] = React.useState(false);
-  const [showFailedModal, setShowFailedModal] = React.useState(true);
+  const [showFailedModal, setShowFailedModal] = React.useState(false);
   const [tab, setTab] = React.useState(1);
   const { isMobile } = useWindowSize();
+  const [submitLoading, setSubmitLoading] = React.useState(false);
 
   useEffect(() => {
     if (!loadingCounterFlag) {
@@ -151,19 +152,23 @@ const ActivityPage = (props: ActivityPageProps) => {
 
   const submit = async () => {
     if (recordedChunks.length) {
+      setSubmitLoading(true);
       const blob = new Blob(recordedChunks, {
         type: "video/webm",
       });
       try {
-        await fetch("http://localhost:8000/upload", {
+        const res = await fetch("http://localhost:8000/upload", {
           method: "POST",
           body: blob,
           headers: {
             "Content-Type": "video/mp4",
           },
         });
+        setSubmitLoading(false);
+        setShowSuccessModal(true);
         setRecordedChunks([]);
       } catch (e) {
+        setShowFailedModal(true);
         console.error("failed");
       }
     }
@@ -223,6 +228,29 @@ const ActivityPage = (props: ActivityPageProps) => {
                 <div className="z-20 text-7xl text-white">{loadingCounter}</div>
               </div>
             )}
+            {submitLoading && (
+              <div className="absolute z-10 flex h-full w-full items-center justify-center bg-gray-800 opacity-75">
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </div>
+            )}
             <Webcam audio={false} mirrored={true} ref={webcamRef} />
           </div>
           <div className="h-8 w-[80%]">
@@ -242,7 +270,13 @@ const ActivityPage = (props: ActivityPageProps) => {
           )}
           {recordedChunks.length > 0 && <Button onClick={reset}>Reset</Button>}
           {recordedChunks.length > 0 && (
-            <Button onClick={submit}>Submit</Button>
+            <Button
+              onClick={() => {
+                submit();
+              }}
+            >
+              Submit
+            </Button>
           )}
         </div>
       )}
